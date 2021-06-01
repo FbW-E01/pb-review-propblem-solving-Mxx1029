1c56f5c2aa084daaba8d07446da5041d2d90bfbe
Mxx1029 <lia.pim@icloud.com>
2021-06-01 14:43:32 +0200

    Initial commit

---
+++ question1.js
@@ -0,0 +1,28 @@
+// 1. Make a function that works like this:
+
+//         examine({})         // ["object"]
+//         examine("hi")       // ["string"]
+//         examine(3, 1)       // ["number", "number"]
+//         examine(3, "hi")    // ["number", "string"]
+//         examine([], ()=>{}) // ["array", "function"]
+
+
+// estimation: 30 min (start 2:22) ==> stop 2:42 -> 20 mins (time estimate was pretty good in a business sense ;))
+
+const examine = (...input) => {
+    const output = [];
+    for (let i = 0; i < input.length; i++) {
+        if (typeof input[i] === "object") {
+            if (isNaN(input[i]) === true) {
+                output.push("array");
+            } else {
+                output.push("array")
+            };
+        } else {
+            output.push(typeof input[i]);
+        };
+    };
+    return output;
+};
+
+console.log(examine([], ()=>{}));
